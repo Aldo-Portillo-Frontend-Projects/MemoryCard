ad3552c4102bf04b771d229ccfd52cfd544958eb
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-29 16:49:30 -0500

    Remove boilerplate and create basic card components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card';
 
 function App() {
+
+  function clickCard() {
+    console.log("I have been clicked")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Memory Card Game</h1>
+      <div className='card-container'>
+        <Card input="rendered card"/>
+        <Card input="rendered card"/>
+        <Card input="rendered card"/>
+        <Card input="rendered card"/>
+      </div>
     </div>
   );
 }
