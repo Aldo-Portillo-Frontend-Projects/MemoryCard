2c12bb38865c5b29982f5b0981f3be28bd7149b6
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-30 11:24:09 -0500

    Create a function that returns data so new data can be called using state

--- src/App.js
+++ src/App.js
@@ -8,19 +8,39 @@ function App() {
   
   const [score, setScore] = React.useState(0)
 
-  const data1 = [
-    {key: nanoid(), value: "card one", isClicked: false},
-    {key: nanoid(), value: "card two", isClicked: false},
-    {key: nanoid(), value: "card three", isClicked: false},
-    {key: nanoid(), value: "card four", isClicked: false},
-    {key: nanoid(), value: "card five", isClicked: false},
-    {key: nanoid(), value: "card six", isClicked: false},
-    {key: nanoid(), value: "card seven", isClicked: false},
-    {key: nanoid(), value: "card eight", isClicked: false},
-  ]
+  const [level, setLevel] = React.useState(1) //USe state to check is clicked
 
+  function levels (level) {
+    if (level === 1) {
+      return [
+        {key: nanoid(), value: "card one", isClicked: false},
+        {key: nanoid(), value: "card two", isClicked: false},
+        {key: nanoid(), value: "card three", isClicked: false},
+        {key: nanoid(), value: "card four", isClicked: false},
+        {key: nanoid(), value: "card five", isClicked: false},
+        {key: nanoid(), value: "card six", isClicked: false},
+        {key: nanoid(), value: "card seven", isClicked: false},
+        {key: nanoid(), value: "card eight", isClicked: false},
+      ]
+    } else if (level === 2) {
+      return [
+        {key: nanoid(), value: "card one", isClicked: false},
+        {key: nanoid(), value: "card two", isClicked: false},
+        {key: nanoid(), value: "card three", isClicked: false},
+        {key: nanoid(), value: "card four", isClicked: false},
+        {key: nanoid(), value: "card five", isClicked: false},
+        {key: nanoid(), value: "card six", isClicked: false},
+        {key: nanoid(), value: "card seven", isClicked: false},
+        {key: nanoid(), value: "card eight", isClicked: false},
+        {key: nanoid(), value: "card nine", isClicked: false},
+        {key: nanoid(), value: "card ten", isClicked: false},
+        {key: nanoid(), value: "card eleven", isClicked: false},
+        {key: nanoid(), value: "card twelve", isClicked: false},
+      ]
+    }
+  }
 
-  const [data, setData] = React.useState(data1)
+  const [data, setData] = React.useState(levels(2))
 
 
   function modifyScore(id, isClicked) {
