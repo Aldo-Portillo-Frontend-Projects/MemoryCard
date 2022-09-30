0370c71912c13d48976dc937514d413520c4eeb8
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-30 11:17:25 -0500

    Make score reset to 0 if a already selected card is clicked

--- src/App.js
+++ src/App.js
@@ -23,19 +23,27 @@ function App() {
   const [data, setData] = React.useState(data1)
 
 
-  function modifyScore(id) {
-    setScore(prevScore => prevScore + 1)
+  function modifyScore(id, isClicked) {
+
+    if(isClicked === false){
+      setScore(prevScore => prevScore + 1)
+    } else {
+      setScore(0)
+    }
+
+
     setData(prevData => prevData.map(card => {
       return card.key === id ? {...card, isClicked: true} : card  ;
-      
     }))
+
+
     console.log(id)
   }
 
   console.log(data)
 
   const dataRender = data.map(card => {
-    return <Card key={card.key} input={card.value} clickHandle={() => modifyScore(card.key)} />
+    return <Card key={card.key} input={card.value} clickHandle={() => modifyScore(card.key, card.isClicked)} />
   })
 
   return (
